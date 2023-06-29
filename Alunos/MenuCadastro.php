41ab64fd2dc06ac3f060f517733aef3a3563bce5
Lucas aurnheimer <99219845+LucasAurnheimer@users.noreply.github.com>
2023-06-29 10:09:03 -0300

    Update and rename criar.php to MenuCadastro.php

---
+++ Alunos/MenuCadastro.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRUD de Alunos</title>
+</head>
+
+<body>
+    <h1>CRUD de Alunos</h1>
+    <ul>
+        <li><a href="incluir.php">Incluir Aluno</a></li>
+        <li><a href="alterar.php">Alterar Aluno</a></li>
+        <li><a href="listarTodos.php">Listar Alunos</a></li>
+        <li><a href="listarUm.php">Listar um Aluno</a></li>
+        <li><a href="excluirAluno.php">Excluir Aluno</a></li>
+    </ul>
+</body>
+
+</html>
