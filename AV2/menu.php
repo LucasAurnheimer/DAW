1e8d845830e9fc5f1d9aed119c687c01a22b8a7b
Lucas aurnheimer <99219845+LucasAurnheimer@users.noreply.github.com>
2023-07-04 11:59:21 -0300

    Create menu.php

---
+++ AV2/menu.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Menu de informações gerais</title>
+</head>
+
+<body>
+    <h1>Quais informações deseja consultar: </h1>
+    <ul>
+        <li><a href="incluir.php">Cadastrar Candidato</a></li>
+        <li><a href="incluirFiscal.php">Incluir Fiscal</a></li>
+        <li><a href="listagem.php">Listar Candidatos por sala</a></li>
+        <li><a href="alterar.php">Alterar Candidato de Sala</a></li>
+    </ul>
+</body>
+
+</html>
