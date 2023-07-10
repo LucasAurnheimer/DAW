dd02bc52f4a61e1e512a190d8c73547ed1eb150a
Lucas aurnheimer <99219845+LucasAurnheimer@users.noreply.github.com>
2023-07-10 20:41:15 -0300

    Update incluir.php
    adicionei um link para voltar ao menu

--- AV2/incluir.php
+++ AV2/incluir.php
@@ -5,45 +5,53 @@ $user = "root";
 $pass = "";
 $db = "candidatos";
 
-
 $mysqli = mysqli_connect($host, $user, $pass, $db);
 
 if (!$mysqli) {
     die("Erro ao conectar ao banco de dados: " . mysqli_connect_error());
 }
 
+function contarCandidatosSala($sala, $mysqli) {
+    $sql_count = "SELECT COUNT(*) AS total FROM candidatosinfo WHERE sala = '$sala'";
+    $result_count = mysqli_query($mysqli, $sql_count);
+    $row_count = mysqli_fetch_assoc($result_count);
+    $total = $row_count['total'];
+
+    return $total;
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-
-     $salas = ['1', '2', '3', '4', '5'];
-     $numSalas = count($salas);
-   
+    $salas = ['1', '2', '3', '4', '5'];
     $candidatos = $_POST['candidatos'];
-  
+
     foreach ($candidatos as $candidato) {
         $nome = $candidato['nome'];
         $cpf = $candidato['cpf'];
-        $Rg = $candidato['Rg'];
+        $rg = $candidato['Rg'];
         $email = $candidato['email'];
-		$cargo = $candidato['cargo'];
-		$sala = $salas[array_rand($salas)];
+        $cargo = $candidato['cargo'];
 
-        $sql = "INSERT INTO candidatosinfo (nome, cpf, Rg, email, cargo, sala) VALUES ('$nome', '$cpf', '$Rg','$email', '$cargo','$sala')";
-        $resultado = mysqli_query($mysqli, $sql);
+        $salaDisponivel = $salas[array_rand($salas)];
+        $totalCandidatosSala = contarCandidatosSala($salaDisponivel, $mysqli);
 
-    
-        if (!$resultado) {
-            echo "Erro ao cadastrar candidato: " . mysqli_error($mysqli);
+        if ($totalCandidatosSala < 50) {
+            $sql = "INSERT INTO candidatosinfo (nome, cpf, Rg, email, cargo, sala) VALUES ('$nome', '$cpf', '$rg','$email', '$cargo','$salaDisponivel')";
+            $resultado = mysqli_query($mysqli, $sql);
+
+            if (!$resultado) {
+                echo "Erro ao cadastrar candidato: " . mysqli_error($mysqli);
+                break;
+            }
+        } else {
+            echo "Não há vagas disponíveis para cadastrar o candidato.";
             break;
         }
     }
 
-
     if ($resultado) {
-        echo "candidatos cadastrados com sucesso!";
+        echo "Candidatos cadastrados com sucesso!";
     }
 }
-
 
 mysqli_close($mysqli);
 
@@ -56,14 +64,12 @@ mysqli_close($mysqli);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
 </head>
 
 <body>
     <form method="post">
 
         <h2>Adicionar candidatos:</h2>
-		
 
         <div id="candidatos-container">
             <div class="aluno">
@@ -71,56 +77,31 @@ mysqli_close($mysqli);
                 <input type="text" name="candidatos[0][nome]" required>
 
                 <label for="cpf"> CPF:</label>
-                <input type="text" name="candidatos[0][cpf]" required>
+                <input type="text" name="candidatos[0][cpf]" required placeholder="Digite 9 dígitos">
 
                 <label for="Rg">Rg:</label>
-                <input type="text" name="candidatos[0][Rg]" required>
+                <input type="text" name="candidatos[0][Rg]" required placeholder="Digite 9 dígitos">
 
                 <label for="email">email:</label>
-                <input type="text" name="candidatos[0][email]" required>
-				<br>
-				<br>
-				<label for="email">Cargo:</label>
+                <input type="text" name="candidatos[0][email]" required placeholder="example@.com">
+                <br>
+                <br>
+                <label for="email">Cargo:</label>
                 <input type="text" name="candidatos[0][cargo]" required>
-				<br>
-				
-			 <h3> a sala que o candidato fara a prova será determinada em breve </h3>
+                <br>
+
+                <h3>A sala que o candidato fará a prova será determinada em breve</h3>
             </div>
         </div>
 
         <input type="submit" value="Cadastrar">
+        <br>
+        <br>
+        <a href="menu.php">Voltar ao Menu:</a>
+
     </form>
 
-    <script>
-    let contador = 0;
-    const candidatosContainer = document.getElementById('candidatos-container');
-
-    function addCandidato() {
-        contador++;
-        const div = document.createElement('div');
-        div.classList.add('candidato');
-        div.innerHTML = `
-		
-                <label for ="nome">Nome:</label>
-                <input type="text" name="candidatos[${contador}][nome]" required>
-			
-                <label for="cpf">CPF:</label>
-                <input type="text" name="candidatos[${contador}][cpf]" required>
-
-                <label for="Rg">Rg:</label>
-                <input type="text" name="candidatos[${contador}][Rg]" required>
-
-                <label for="email">email:</label>
-                <input type="text" name="candidatos[${contador}][email]" required>
-				
-				
-				<label for="cargo">Cargo:</label>
-                <input type="text" name="candidatos[${contador}][cargo]" required>
-            `;
-        candidatosContainer.appendChild(div);
-    }
-
-    </script>
+    
 </body>
 
 </html>
